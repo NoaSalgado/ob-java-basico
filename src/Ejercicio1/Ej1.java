d4bf27f920e7434eed899fb92acb70dc26000366
Noa <noasalgadodev@gmail.com>
2022-07-12 13:55:20 +0200

    Added exercise 1

---
+++ src/Ejercicio1/Ej1.java
@@ -0,0 +1,17 @@
+package Ejercicio1;
+
+public class Ej1 {
+    public static void main(String[] args) {
+        int number = 5;
+        long longNumber = 1234567894L;
+        double decimalNumber = 267.5;
+        boolean isDevelover = true;
+        String text = "Hello world!";
+
+        System.out.println("El número " + number + " es de tipo int");
+        System.out.println("El número " + longNumber + " es de tipo long");
+        System.out.println("El número " + decimalNumber + " es de tipo double");
+        System.out.println("¿Soy desarrolladora? " + isDevelover);
+        System.out.println(text);
+    }
+}
